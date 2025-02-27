5fd37d79b52abcb40b3969cc83ae70e77d7dade3
bayy <bayusubekti00@gmail.com>
2025-02-27 15:36:00 +0700

    Feat: Checkout Page

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Checkout')]
 class CheckoutPage extends Component
 {
+    public $first_name, $last_name, $phone, $street_address, $city, $state, $zip_code, $payment_method;
+
+    public function placeOrder(){
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'street_address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip_code' => 'required',
+            'payment_method' => 'required'
+            ]);
+    }
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+        return view('livewire.checkout-page', [
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,10 @@ Route::get('/products/{slug}', ProductDetailPage::class);
 Route::middleware('guest')->group(function () {
     Route::get('/login', LoginPage::class)->name('login');
     Route::get('/register', RegisterPage::class);
-    Route::get('/forgot', ForgotPasswordPage::class);
-    Route::get('/reset', ResetPasswordPage::class);
+    Route::get('/forgot', ForgotPasswordPage::class)->name('password.request');
+    Route::get('/reset/{token}', ResetPasswordPage::class)->name('password.reset');
 });
-
+ 
 Route::middleware('auth')->group(function () {
     Route::get('/logout', function(){
         Auth::logout();
